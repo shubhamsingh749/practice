f855295a5bd79c34af4205456beb254388c70525
Shubham Singh <roman.shubh.reigns@gmail.com>
2023-11-10 17:31:50 -0800

    update

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Responsive from './Responsive';
+
+function App() {
+  return (
+   <Responsive/>
+  );
+}
+
+export default App;
---
+++ src/Responsive.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Responsive.css"
+const Responsive = () => {
+  return (
+    <div className="image-container">
+      <img src="/tuxpi.com.1699615423.jpg" alt="Image of a blackboard with a bunch of sports equipment on it" />
+    </div>
+  );
+};
+
+export default Responsive;
